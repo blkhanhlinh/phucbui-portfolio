e199bd6577594525597b0de4caaeeb55f15a9220
blkhanhlinh <linhbui8103@gmail.com>
2023-01-26 02:24:16 +0700

    fix view archive button

--- src/components/view-archive/index.js
+++ src/components/view-archive/index.js
@@ -5,20 +5,14 @@ import ViewArchiveButton from "../../assets/images/view-archive.svg";
 import classNames from "classnames";
 import React from "react";
 
-const Button = React.forwardRef(({ onClick, href }, ref) => {
-    return (
-        <a href={href} onClick={onClick} ref={ref} className={classNames(styles.viewButton)}>                    
-            <Image src={ViewArchiveButton} alt="view archive" width={164} height={161} />
-        </a>
-    )
-})
-
 export default function ViewArchive() {
     return (
         <>
             <div>
                 <Link href="/archive">
-                    <Button />
+                    <a className={classNames(styles.viewButton)}>
+                        <Image src={ViewArchiveButton} alt="view archive" width={164} height={161} />
+                    </a>                
                 </Link>
             </div>
         </>
