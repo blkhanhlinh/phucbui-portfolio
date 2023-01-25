974acc21940199de3a58ed8da75c317d504b9cca
blkhanhlinh <linhbui8103@gmail.com>
2023-01-26 02:19:15 +0700

    add favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,6 +7,8 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta charSet="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.png" />
+        <title>Phuc Bui</title>
       </Head>
       <Layout>
         <Component {...pageProps} />
