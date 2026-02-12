9a53db6b8926e76592ee404c2be03225c71bba89
blkhanhlinh <lilahbui.work@gmail.com>
2026-02-12 20:07:14 +0700

    redirect to figma until it done

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination:
+          'https://www.figma.com/proto/LYCHX7sPqp7SFRoAJ5YssF/Portfolio-Website?page-id=629%3A4599&node-id=1998-265&viewport=-655%2C20557%2C0.28&t=ROXGnJrwWvwM3HUB-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=1549%3A704&show-proto-sidebar=1',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
